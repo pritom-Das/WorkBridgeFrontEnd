60addf4b9e1e9573585a302c511136f756571561
MahmudulHaqueRahat <rahat23fahat@gmail.com>
2026-01-19 22:52:11 +0600

    Theme adjast

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -76,7 +76,7 @@ export default function Navbar() {
   };
 
   return (
-    <div className="navbar bg-base-100 shadow-sm px-4 lg:px-8">
+    <div className="navbar bg-base-100 shadow-sm px-4 lg:px-8" data-theme="dark">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
