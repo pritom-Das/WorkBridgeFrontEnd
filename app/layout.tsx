289239d927a2b45ee55e0e3413f8a258e0c3e899
MahmudulHaqueRahat <rahat23fahat@gmail.com>
2026-01-20 04:01:57 +0600

    Done2

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
        <Navbar />               
-        <main className="flex-1">{children}</main> 
+        <main className=" flex flex-col">{children}</main> 
         <Footer />
       </body>
     </html>
